64965cb3e9284c1cc971c08eb6e8164a3c075f30
Pavel Raunou <praunou@paloaltonetworks.com>
2024-09-16 12:19:38 +0200

    Changing default create_private_ipv6->true

--- modules/vmseries/main.tf
+++ modules/vmseries/main.tf
@@ -53,7 +53,7 @@ resource "google_compute_address" "private" {
 }
 
 resource "google_compute_address" "private_v6" {
-  for_each = { for k, v in var.network_interfaces : k => v if try(v.stack_type, "IPV4_ONLY") == "IPV4_IPV6" && try(v.create_private_ipv6, false) == true }
+  for_each = { for k, v in var.network_interfaces : k => v if try(v.stack_type, "IPV4_ONLY") == "IPV4_IPV6" && try(v.create_private_ipv6, true) == true }
 
   name         = try(each.value.private_ipv6_name, "${var.name}-${each.key}-private-v6")
   address_type = "INTERNAL"
