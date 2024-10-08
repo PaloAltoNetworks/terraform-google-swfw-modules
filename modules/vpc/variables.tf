45a5a22e90da044c337c39917bdc9d76ec9f19a4
Horia Gunica <Horia.Gunica@gmail.com>
2024-10-08 17:24:32 +0300

    Added validation logic for log_config section

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -64,6 +64,16 @@ variable "subnetworks" {
       filter_expr          = optional(string)
     }))
   }))
+  validation {
+    condition = alltrue([
+      for subnet in var.subnetworks : 
+        subnet.log_config != null ? (
+          (subnet.log_config.aggregation_interval != null && can(regex("^INTERVAL_(5_SEC|30_SEC|1_MIN|5_MIN|10_MIN|15_MIN)$", subnet.log_config.aggregation_interval)) ? true : false) &&
+          (subnet.log_config.metadata != null && can(regex("^(EXCLUDE_ALL_METADATA|INCLUDE_ALL_METADATA|CUSTOM_METADATA)$", subnet.log_config.metadata)) ? true : false)
+        ) : true
+    ])
+    error_message = "If log_config is specified, aggregation_interval must be one of INTERVAL_5_SEC, INTERVAL_30_SEC, INTERVAL_1_MIN, INTERVAL_5_MIN, INTERVAL_10_MIN, INTERVAL_15_MIN, and metadata must be one of EXCLUDE_ALL_METADATA, INCLUDE_ALL_METADATA, or CUSTOM_METADATA."
+  }
 }
 
 variable "firewall_rules" {
