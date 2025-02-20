fe8c63b85967248b9ec1f0ca2bd09fb0c73eaa3b
Gustavo Franco <gfrancocarn@paloaltonetworks.com>
2025-02-20 13:16:51 -0300

    fix: missing description to project variable

--- modules/vmseries/variables.tf
+++ modules/vmseries/variables.tf
@@ -4,6 +4,7 @@ variable "name" {
 }
 
 variable "project" {
+  description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used"
   default = null
   type    = string
 }
