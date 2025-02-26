1076c833e043d8a5a7e9b3aa834734d9e88a9c0d
Gustavo Franco <gfrancocarn@paloaltonetworks.com>
2025-02-26 18:48:47 -0300

    chore: add resource docs links

--- modules/iam_service_account/main.tf
+++ modules/iam_service_account/main.tf
@@ -1,9 +1,11 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
 resource "google_service_account" "this" {
   account_id   = var.service_account_id
   display_name = var.display_name
   project      = var.project_id
 }
 
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam#google_project_iam_member-1
 resource "google_project_iam_member" "this" {
   for_each = var.roles
 
