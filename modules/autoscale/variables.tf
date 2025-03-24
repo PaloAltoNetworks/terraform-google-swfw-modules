629aadc652180e76c3e732fb2c4858034f2ede1d
Gustavo Franco <gfrancocarn@paloaltonetworks.com>
2025-03-24 17:06:20 +0000

    feat: autoscaler_metrics type definition

--- modules/autoscale/variables.tf
+++ modules/autoscale/variables.tf
@@ -179,14 +179,22 @@ variable "max_vmseries_replicas" {
 
 variable "autoscaler_metrics" {
   description = "A map with the keys being metrics identifiers (e.g. custom.googleapis.com/VMSeries/panSessionUtilization). Each of the contained objects has attribute `target` which is a numerical threshold for a scale-out or a scale-in. Each zonal group grows until it satisfies all the targets. Additional optional attribute `type` defines the metric as either `GAUGE`, `DELTA_PER_SECOND`, or `DELTA_PER_MINUTE`. For full specification, see the `metric` inside the [provider doc](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler)."
+  type = map(
+    object(
+      {
+        target = number
+        type   = optional(string, "GAUGE")
+      }
+    )
+  )
   default = {
     "custom.googleapis.com/VMSeries/panSessionUtilization" = {
       target = 70,
-      type = "GAUGE"
+      type   = "GAUGE"
     }
     "custom.googleapis.com/VMSeries/panSessionThroughputKbps" = {
       target = 700000,
-      type = "GAUGE"
+      type   = "GAUGE"
     }
   }
 }
