f6f890826e587210e0e36e64672ed9f538177de1
Gustavo Franco <gfrancocarn@paloaltonetworks.com>
2025-03-24 17:07:41 +0000

    docs: mention type default value

--- modules/autoscale/variables.tf
+++ modules/autoscale/variables.tf
@@ -178,7 +178,7 @@ variable "max_vmseries_replicas" {
 }
 
 variable "autoscaler_metrics" {
-  description = "A map with the keys being metrics identifiers (e.g. custom.googleapis.com/VMSeries/panSessionUtilization). Each of the contained objects has attribute `target` which is a numerical threshold for a scale-out or a scale-in. Each zonal group grows until it satisfies all the targets. Additional optional attribute `type` defines the metric as either `GAUGE`, `DELTA_PER_SECOND`, or `DELTA_PER_MINUTE`. For full specification, see the `metric` inside the [provider doc](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler)."
+  description = "A map with the keys being metrics identifiers (e.g. custom.googleapis.com/VMSeries/panSessionUtilization). Each of the contained objects has attribute `target` which is a numerical threshold for a scale-out or a scale-in. Each zonal group grows until it satisfies all the targets. Additional optional attribute `type` defines the metric as either `GAUGE`, `DELTA_PER_SECOND`, or `DELTA_PER_MINUTE` - Defaults to `GAUGE`. For full specification, see the `metric` inside the [provider doc](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler)."
   type = map(
     object(
       {
